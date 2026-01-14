d6cf5e3598c7812cf088a66630e5f4186a76a93b
HoracioxBarrios <horacioxbarrios@gmail.com>
2026-01-13 22:29:25 -0300

    avances en header / perfil: configutacion de mixins para estilos responsive; estructura del mock de userInfo + cambio en el html para redes sociales

---
+++ src/app/core/models/redSocial.ts
@@ -0,0 +1,5 @@
+export interface RedSocial {
+  label: string;
+  link: string;
+  icon: string;
+}
--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -5,7 +5,7 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
 import { JoinTextPipe } from '../../pipes/join-text-pipe';
-
+import {RedSocial} from "../../../core/models/redSocial"
 @Component({
   selector: 'app-header',
   imports: [
@@ -21,38 +21,47 @@ import { JoinTextPipe } from '../../pipes/join-text-pipe';
 })
 export class Header {
 
-private userInfo = {
-  name : "Horacio",
-  secondName: "Javier",
-  lastName : "Barrios",
-  tecnologias: ["Desarrollador FullStack", "Html", "CSS", "Typescript", "Angular"],
-  location: "Buenos Aires - Argentina",
-  photo: 'images/user-profile.webp'
-}
+  private userInfo = {
+    name : "Horacio",
+    secondName: "Javier",
+    lastName : "Barrios",
+    tecnologias: ["Desarrollador FullStack", "Html", "CSS", "Typescript", "Angular"],
+    location: "Buenos Aires - Argentina",
+    photo: 'images/user-profile.webp',
+    redesSociales: [
+      { label: 'GitHub', link: 'https://github.com/HoracioxBarrios', icon: 'code' },
+      { label: 'LinkedIn', link: 'https://linkedin.com/in/horacioxbarrios', icon: 'work' },
+      { label: 'Instagram', link: 'https://instagram.com', icon: 'photo_camera' }
+    ]
+
+  }
 
 
-navLinks = [
-  { id: 1, path: '/home', label: 'Inicio' },
-  { id: 2, path: '/proyectos', label: 'Proyectos' },
-  { id: 3, path: '/publicaciones', label: 'Publicaciones' },
-  { id: 4, path: '/sobre-mi', label: 'Sobre Mí' }
-];
+  navLinks = [
+    { id: 1, path: '/proyectos', label: 'Proyectos' },
+    { id: 2, path: '/publicaciones', label: 'Publicaciones' },
+    { id: 3, path: '/sobre-mi', label: 'Sobre Mí' }
+  ];
 
 
-get fullName(): string {
-  return [this.userInfo.name, this.userInfo.secondName, this.userInfo.lastName]
-    .filter(Boolean)
-    .join(' ');
-}
+  get fullName(): string {
+    return [this.userInfo.name, this.userInfo.secondName, this.userInfo.lastName]
+      .filter(Boolean)
+      .join(' ');
+  }
 
-get tecnologiasArr():any[]{
-  return this.userInfo.tecnologias;
-}
-get location(){
-  return this.userInfo.location;
-}
-get photo(){
-  return this.userInfo.photo;
-}
+  get tecnologiasArr():any[]{
+    return this.userInfo.tecnologias;
+  }
+  get location(){
+    return this.userInfo.location;
+  }
+  get photo(){
+    return this.userInfo.photo;
+  }
+
+  get redesSociales(): RedSocial[] {
+    return this.userInfo?.redesSociales ?? [];
+  }
 
 }
