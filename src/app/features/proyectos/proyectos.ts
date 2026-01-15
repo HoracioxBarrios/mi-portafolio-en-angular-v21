831e2c105dae3a00bac348e3e2223379176b75b4
HoracioxBarrios <horacioxbarrios@gmail.com>
2026-01-15 00:51:40 -0300

    avance en la seccion proyectos

--- src/app/features/proyectos/proyectos.ts
+++ src/app/features/proyectos/proyectos.ts
@@ -19,11 +19,39 @@ export class Proyectos {
       liveUrl: 'https://www.thecolvinco.com/'
     },
     {
-      title: 'Proyecto Demo',
-      description: 'Aplicación demo con Angular y Material.',
-      image: 'images/projects/project-2.webp',
+      title: 'Viajeros con B',
+      description: 'Viajeros con B es una plataforma de contenido de viajes vinculada a B travel, enfocada en inspirar a los usuarios a través de artículos, experiencias y recomendaciones sobre destinos, turismo y estilos de viaje, presentada en formato editorial y audiovisual.',
+      image: 'images/viajeros_con_B_web_de_viajes.webp',
       repoUrl: 'https://github.com/usuario/demo',
-      liveUrl: 'https://demo.com'
+      liveUrl: 'https://viajerosconb.btravel.com/'
+    },
+        {
+      title: 'Calvin',
+      description: 'TheColvinCo.com es una tienda online especializada en la venta y envío de flores, plantas y regalos a domicilio, con enfoque en ramos y arreglos florales para ocasiones especiales como cumpleaños, aniversarios, celebraciones, fechas señaladas y momentos de detalle.',
+      image: 'images/calvin_web_venta_flores.webp',
+      repoUrl: 'https://github.com/Horacioxbarrios',
+      liveUrl: 'https://www.thecolvinco.com/'
+    },
+    {
+      title: 'Viajeros con B',
+      description: 'Viajeros con B es una plataforma de contenido de viajes vinculada a B travel, enfocada en inspirar a los usuarios a través de artículos, experiencias y recomendaciones sobre destinos, turismo y estilos de viaje, presentada en formato editorial y audiovisual.',
+      image: 'images/viajeros_con_B_web_de_viajes.webp',
+      repoUrl: 'https://github.com/usuario/demo',
+      liveUrl: 'https://viajerosconb.btravel.com/'
+    },
+            {
+      title: 'Calvin',
+      description: 'TheColvinCo.com es una tienda online especializada en la venta y envío de flores, plantas y regalos a domicilio, con enfoque en ramos y arreglos florales para ocasiones especiales como cumpleaños, aniversarios, celebraciones, fechas señaladas y momentos de detalle.',
+      image: 'images/calvin_web_venta_flores.webp',
+      repoUrl: 'https://github.com/Horacioxbarrios',
+      liveUrl: 'https://www.thecolvinco.com/'
+    },
+    {
+      title: 'Viajeros con B',
+      description: 'Viajeros con B es una plataforma de contenido de viajes vinculada a B travel, enfocada en inspirar a los usuarios a través de artículos, experiencias y recomendaciones sobre destinos, turismo y estilos de viaje, presentada en formato editorial y audiovisual.',
+      image: 'images/viajeros_con_B_web_de_viajes.webp',
+      repoUrl: 'https://github.com/usuario/demo',
+      liveUrl: 'https://viajerosconb.btravel.com/'
     }
   
   ];
