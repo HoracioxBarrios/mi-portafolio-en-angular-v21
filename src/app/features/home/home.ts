ac54cdd2448d6857f93b8c219474e7c115eacffd
HoracioxBarrios <horacioxbarrios@gmail.com>
2026-03-05 02:08:08 -0300

    diseño e implementacion:  home

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Skills } from '../../shared/components/skills/skills';
+import { ProjectCard } from '../proyectos/components/project-card/project-card';
+
+interface Project {
+  title: string;
+  description: string;
+  image: string;
+  repoUrl?: string;
+  liveUrl?: string;
+}
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Skills, ProjectCard],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {
+export class Home implements OnInit {
+  featuredProjects: Project[] = [];
 
+  constructor() {}
+
+  ngOnInit() {
+    this.loadFeaturedProjects();
+  }
+
+  private loadFeaturedProjects() {
+    // TODO: Integrar con ProjectsService para obtener proyectos destacados
+    // this.projectsService.getFeaturedProjects().subscribe(projects => {
+    //   this.featuredProjects = projects.slice(0, 3);
+    // });
+    
+    // Datos temporales para visualización
+    this.featuredProjects = [];
+  }
 }
