a4ec5101e1111fd561c2b53f37df41364438ebad
HoracioxBarrios <horacioxbarrios@gmail.com>
2026-03-05 23:42:03 -0300

    ajustes en el home

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -19,6 +19,31 @@ interface Project {
 export class Home implements OnInit {
   featuredProjects: Project[] = [];
 
+    projects = [
+    {
+      title: 'Calvin',
+      description: 'TheColvinCo.com es una tienda online especializada en la venta y envío de flores, plantas y regalos a domicilio, con enfoque en ramos y arreglos florales para ocasiones especiales como cumpleaños, aniversarios, celebraciones, fechas señaladas y momentos de detalle.',
+      image: 'images/calvin_web_venta_flores.webp',
+      repoUrl: 'https://github.com/Horacioxbarrios',
+      liveUrl: 'https://www.thecolvinco.com/'
+    },
+    {
+      title: 'Viajeros con B',
+      description: 'Viajeros con B es una plataforma de contenido de viajes vinculada a B travel, enfocada en inspirar a los usuarios a través de artículos, experiencias y recomendaciones sobre destinos, turismo y estilos de viaje, presentada en formato editorial y audiovisual.',
+      image: 'images/viajeros_con_B_web_de_viajes.webp',
+      repoUrl: 'https://github.com/usuario/demo',
+      liveUrl: 'https://viajerosconb.btravel.com/'
+    },
+        {
+      title: 'Calvin',
+      description: 'TheColvinCo.com es una tienda online especializada en la venta y envío de flores, plantas y regalos a domicilio, con enfoque en ramos y arreglos florales para ocasiones especiales como cumpleaños, aniversarios, celebraciones, fechas señaladas y momentos de detalle.',
+      image: 'images/calvin_web_venta_flores.webp',
+      repoUrl: 'https://github.com/Horacioxbarrios',
+      liveUrl: 'https://www.thecolvinco.com/'
+    }
+  
+  ];
+
   constructor() {}
 
   ngOnInit() {
@@ -32,6 +57,6 @@ export class Home implements OnInit {
     // });
     
     // Datos temporales para visualización
-    this.featuredProjects = [];
+    this.featuredProjects = this.projects ?? [];
   }
 }
