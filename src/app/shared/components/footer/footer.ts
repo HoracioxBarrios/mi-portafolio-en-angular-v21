a9e0924e902dace044f21d8be8e887263579612a
HoracioxBarrios <horacioxbarrios@gmail.com>
2026-06-21 02:17:46 -0300

    ajueste en footer

--- src/app/shared/components/footer/footer.ts
+++ src/app/shared/components/footer/footer.ts
@@ -13,8 +13,6 @@ export class Footer {
 
   // Crédito de diseño (Figma Community) de Ricardo Quebrada R.
   protected readonly designAuthor = 'Ricardo Quebrada R.';
-  protected readonly figmaUrl =
-    'https://www.figma.com/design/0j4mdLvT2tUnB3h6m4iFI1/Portafolio-Profesional--Community-?node-id=196-729';
 
   readonly text = computed(() => `${this.tr.t('footer.text')} ${this.year}`);
 }
